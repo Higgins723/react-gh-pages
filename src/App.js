521a99ec6d511234186b0d9df5be71a437f288b1
higgins723 <xxhiggins723xx@gmail.com>
2019-05-12 20:07:19 -0600

    parallax home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import './App.scss';
-import Navigation from './components/Navigation';
 import Home from './components/Home';
-import Projects from './components/Projects';
 
 const App = () => {
   return (
-    <HashRouter>
-      <div>
-        <Navigation />
-        <Route path="/" exact component={Home} />
-        <Route path="/projects" component={Projects} />
-      </div>
-    </HashRouter>
+    <ParallaxProvider>
+      <Home />
+    </ParallaxProvider>
   )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { ParallaxBanner } from 'react-scroll-parallax';
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <ParallaxBanner
+      className="home-image"
+      layers={[
+        {
+            image: 'https://www.hallevans.com/wp-content/uploads/2019/02/SLC_Media.jpg',
+            amount: 0.4,
+        },
+      ]}
+      style={{
+        height: '650px',
+      }}
+    >
+      <div className="name-style">
+        <h3>Hello, I'm</h3>
+        <h1>Josh Higgins</h1>
+      </div>
+    </ParallaxBanner>
   )
 }
 
