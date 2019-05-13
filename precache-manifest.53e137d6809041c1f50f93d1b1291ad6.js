74ab97289a2c3405644be4e99a443ebca8909d88
higgins723 <xxhiggins723xx@gmail.com>
2019-05-12 20:08:15 -0600

    Updates

---
+++ precache-manifest.53e137d6809041c1f50f93d1b1291ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64180a0de10fba88e6d83550e882f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aecfe94f2d704a8baa65",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "4d0cdeed44e4c2f0ba98",
+    "url": "/static/css/main.ac18a614.chunk.css"
+  },
+  {
+    "revision": "aecfe94f2d704a8baa65",
+    "url": "/static/js/2.f9591bbe.chunk.js"
+  },
+  {
+    "revision": "4d0cdeed44e4c2f0ba98",
+    "url": "/static/js/main.98f9db8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
