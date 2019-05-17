725b1c88e40e6ef5cf7b705e3b9f0ffec72eb5d1
Joshua-Higgins <joshua.higgins@rakuten.com>
2019-05-17 16:13:52 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.463b1a1bf3c5901b0882e94bc78292f0.js"
+  "/precache-manifest.8aee66530dea839f8d7dd3bcad1964c2.js"
 );
 
 self.addEventListener('message', (event) => {
